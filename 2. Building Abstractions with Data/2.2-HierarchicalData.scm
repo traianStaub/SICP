4997687346508d806d59e83a9efb627a3542293c
Traian Staub <37111262+traianStaub@users.noreply.github.com>
2021-03-31 09:47:14 +0300

    Update 2.2-HierarchicalData.scm

--- 2. Building Abstractions with Data/2.2-HierarchicalData.scm
+++ 2. Building Abstractions with Data/2.2-HierarchicalData.scm
@@ -238,3 +238,17 @@
       (cons (accumulate op init (map car seqs))
             (accumulate-n op init (map cdr seqs)))))
 
+;2.37 - matrix operations
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (row) (dot-product row v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m))) 
+
