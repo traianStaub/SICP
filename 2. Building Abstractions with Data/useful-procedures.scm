775c9c582b670b139b4bce664de4f61c665cf6de
Traian Staub <37111262+traianStaub@users.noreply.github.com>
2021-03-31 09:48:07 +0300

    Create useful-procedures.scm

---
+++ 2. Building Abstractions with Data/useful-procedures.scm
@@ -0,0 +1,12 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init sequence) 
+   (define nil '()) 
+   (if (null? (car sequence)) 
+       nil 
+       (cons (accumulate op init (map car sequence)) 
+             (accumulate-n op init (map cdr sequence))))) 
