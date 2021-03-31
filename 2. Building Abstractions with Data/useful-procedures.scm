5f1b6b5731eb631d04fba54403fb495f13c63344
Traian Staub <37111262+traianStaub@users.noreply.github.com>
2021-03-31 10:16:21 +0300

    Update useful-procedures.scm

--- 2. Building Abstractions with Data/useful-procedures.scm
+++ 2. Building Abstractions with Data/useful-procedures.scm
@@ -10,3 +10,16 @@
        nil 
        (cons (accumulate op init (map car sequence)) 
              (accumulate-n op init (map cdr sequence))))) 
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
