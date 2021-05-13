429e3be0fa9eacf908d00d1c054e592bd443361d
Traian Staub <37111262+traianStaub@users.noreply.github.com>
2021-05-13 13:55:50 +0300

    Update Exercises.scm

--- 3. Modularity, Objects and State/Exercises.scm
+++ 3. Modularity, Objects and State/Exercises.scm
@@ -1,8 +1,15 @@
 ;;3.1
-
 (define (make-acc sum)
  (define (add-number number)
    (begin (set! sum (+ sum number)) sum))
   add-number)
 
 (define acc (make-acc 0))
+
+;;3.2
+(define (make-monitored f)
+  (let ((counter 0))
+    (lambda (arg)
+            (cond ((eq? arg 'show) counter)
+                  ((eq? arg'reset) (set! counter 0))
+                  (else (set! counter (+ counter 1)) (f arg))))))
